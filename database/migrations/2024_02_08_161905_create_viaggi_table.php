ccb375d351b1d3a45aec9467d0cd2e56a6f28975
iosonoclaudii <119588458+iosonoclaudii@users.noreply.github.com>
2024-02-09 12:09:08 +0100

    third add

--- database/migrations/2024_02_08_161905_create_viaggi_table.php
+++ database/migrations/2024_02_08_161905_create_viaggi_table.php
@@ -21,11 +21,26 @@ return new class extends Migration
             $table->integer('posti_disponibili');
             // Definizione della colonna "descrizione" come stringa di massimo 255 caratteri per descrivere il viaggio
             $table->string('descrizione', 255);
-            // Aggiunta delle colonne "created_at" e "updated_at" per gestire automaticamente i timestamp di creazione e aggiornamento delle righe
-            $table->timestamps();
+
+            // Definizione della colonna "consumo_carburante" come valore in virgola mobile che rappresenta il consumo di carburante, con la possibilità di essere nullabile
+            $table->float('consumo_carburante')->nullable();
+
+            // Definizione della colonna "emissioni_co2" come valore in virgola mobile che rappresenta le emissioni di CO2, con la possibilità di essere nullabile
+            $table->float('emissioni_co2')->nullable();
+
+            // Definizione della colonna "energia_rinnovabile" come valore booleano che indica se il viaggio coinvolge l'uso di energia rinnovabile, con valore predefinito a falso
+            $table->boolean('energia_rinnovabile')->default(false);
+
+            // Definizione della colonna "trasporto_pubblico" come valore booleano che indica se il viaggio include il trasporto pubblico, con valore predefinito a falso
+            $table->boolean('trasporto_pubblico')->default(false);
+
+            // Definizione della colonna "uso_biciclette" come valore booleano che indica se il viaggio include l'uso di biciclette, con valore predefinito a falso
+            $table->boolean('uso_biciclette')->default(false);
+
 
             // Definizione della chiave esterna "nazione_destinazione" che fa riferimento all'id della tabella "paesi"
             $table->foreign('nazione_destinazione')->references('id')->on('paesi');
+            $table->timestamps();
         });
     }
 
--- database/seeders/ViaggiSeeder.php
+++ database/seeders/ViaggiSeeder.php
@@ -15,7 +15,12 @@ class ViaggiSeeder extends Seeder
             'nome_destinazione' => 'Roma',
             'nazione_destinazione' => 1, // Supponiamo che l'Italia abbia l'ID 1
             'posti_disponibili' => 3,
-            'descrizione' => 'Tour della città di Roma'
+            'descrizione' => 'Visita la città eterna per 3 giorni a 125euro',
+            'consumo_carburante' => (float)rand(0, 10) / 10,
+            'emissioni_co2' => (float)rand(0, 5),
+            'energia_rinnovabile' => (bool)rand(0, 1),
+            'trasporto_pubblico' => (bool)rand(0, 1),
+            'uso_biciclette' => (bool)rand(0, 1),
         ]);
 
         
@@ -23,14 +28,24 @@ class ViaggiSeeder extends Seeder
             'nome_destinazione' => 'Parigi',
             'nazione_destinazione' => 2, // Supponiamo che la Francia abbia l'ID 2
             'posti_disponibili' => 50,
-            'descrizione' => 'Viaggio romantico a Parigi'
+            'descrizione' => 'Esplora la città dell\'amore per una settimana a 200 euro',
+            'consumo_carburante' => (float)rand(0, 10) / 10,
+            'emissioni_co2' => (float)rand(0, 5),
+            'energia_rinnovabile' => (bool)rand(0, 1),
+            'trasporto_pubblico' => (bool)rand(0, 1),
+            'uso_biciclette' => (bool)rand(0, 1),
         ]);
 
         Viaggio::create([
             'nome_destinazione' => 'Madrird',
-            'nazione_destinazione' => 3, // Supponiamo che l'Italia abbia l'ID 1
+            'nazione_destinazione' => 3,
             'posti_disponibili' => 100,
-            'descrizione' => 'Tour della città di Madrid'
+            'descrizione' => 'Scopri la città della movida per un weekend a 150 euro',
+            'consumo_carburante' => (float)rand(0, 10) / 10,
+            'emissioni_co2' => (float)rand(0, 5),
+            'energia_rinnovabile' => (bool)rand(0, 1),
+            'trasporto_pubblico' => (bool)rand(0, 1),
+            'uso_biciclette' => (bool)rand(0, 1),
         ]);
 
 
@@ -38,49 +53,84 @@ class ViaggiSeeder extends Seeder
             'nome_destinazione' => 'Berlino',
             'nazione_destinazione' => 4,
             'posti_disponibili' => 30,
-            'descrizione' => 'Esplorazione culturale a Berlino'
+            'descrizione' => 'Esplora la città dell\'arte moderna per un weekend a 180 euro',
+            'consumo_carburante' => (float)rand(0, 10) / 10,
+            'emissioni_co2' => (float)rand(0, 5),
+            'energia_rinnovabile' => (bool)rand(0, 1),
+            'trasporto_pubblico' => (bool)rand(0, 1),
+            'uso_biciclette' => (bool)rand(0, 1),
         ]);
 
         Viaggio::create([
             'nome_destinazione' => 'Londra',
             'nazione_destinazione' => 5,
             'posti_disponibili' => 25,
-            'descrizione' => 'Tour storico a Londra'
+            'descrizione' => 'Tour della città storica per 5 giorni a 220 euro',
+            'consumo_carburante' => (float)rand(0, 10) / 10,
+            'emissioni_co2' => (float)rand(0, 5),
+            'energia_rinnovabile' => (bool)rand(0, 1),
+            'trasporto_pubblico' => (bool)rand(0, 1),
+            'uso_biciclette' => (bool)rand(0, 1),
         ]);
 
         Viaggio::create([
             'nome_destinazione' => 'New York',
             'nazione_destinazione' => 6,
             'posti_disponibili' => 40,
-            'descrizione' => 'Esperienza metropolitana a New York'
+            'descrizione' => 'Esplora la Grande Mela per una settimana a 250 euro',
+            'consumo_carburante' => (float)rand(0, 10) / 10,
+            'emissioni_co2' => (float)rand(0, 5),
+            'energia_rinnovabile' => (bool)rand(0, 1),
+            'trasporto_pubblico' => (bool)rand(0, 1),
+            'uso_biciclette' => (bool)rand(0, 1),
         ]);
 
         Viaggio::create([
             'nome_destinazione' => 'Tokyo',
             'nazione_destinazione' => 7,
             'posti_disponibili' => 35,
-            'descrizione' => 'Esplorazione culturale a Tokyo'
+            'descrizione' => 'Scopri la città tradizionale per 4 giorni a 200 euro',
+            'consumo_carburante' => (float)rand(0, 10) / 10,
+            'emissioni_co2' => (float)rand(0, 5),
+            'energia_rinnovabile' => (bool)rand(0, 1),
+            'trasporto_pubblico' => (bool)rand(0, 1),
+            'uso_biciclette' => (bool)rand(0, 1),
         ]);
 
         Viaggio::create([
             'nome_destinazione' => 'Pechino',
             'nazione_destinazione' => 8,
             'posti_disponibili' => 20,
-            'descrizione' => 'Tour storico a Pechino'
+            'descrizione' => 'Esplora la Città Proibita per 3 giorni a 150 euro',
+            'consumo_carburante' => (float)rand(0, 10) / 10,
+            'emissioni_co2' => (float)rand(0, 5),
+            'energia_rinnovabile' => (bool)rand(0, 1),
+            'trasporto_pubblico' => (bool)rand(0, 1),
+            'uso_biciclette' => (bool)rand(0, 1),
         ]);
 
         Viaggio::create([
             'nome_destinazione' => 'Rio de Janeiro',
             'nazione_destinazione' => 9,
             'posti_disponibili' => 15,
-            'descrizione' => 'Esperienza tropicale a Rio de Janeiro'
+            'descrizione' => 'Esperienza tropicale per una settimana a 230 euro',
+            'consumo_carburante' => (float)rand(0, 10) / 10,
+            'emissioni_co2' => (float)rand(0, 5),
+            'energia_rinnovabile' => (bool)rand(0, 1),
+            'trasporto_pubblico' => (bool)rand(0, 1),
+            'uso_biciclette' => (bool)rand(0, 1),
         ]);
 
         Viaggio::create([
             'nome_destinazione' => 'New Delhi',
             'nazione_destinazione' => 10,
             'posti_disponibili' => 50,
-            'descrizione' => 'Esplorazione culturale a New Delhi'
+            'descrizione' => 'Tour della città antica per 4 giorni a 180 euro',
+            'consumo_carburante' => (float)rand(0, 10) / 10,
+            'emissioni_co2' => (float)rand(0, 5),
+            'energia_rinnovabile' => (bool)rand(0, 1),
+            'trasporto_pubblico' => (bool)rand(0, 1),
+            'uso_biciclette' => (bool)rand(0, 1),
         ]);
 
     }
